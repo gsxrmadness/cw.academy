e0c4b32670868b1d5785649cea1de3368007e3ac
gsxrmadness <pavel.lamzin@yandex.ru>
2018-12-17 23:42:18 +0300

    initial commit

---
+++ data/career_data.php
@@ -0,0 +1,83 @@
+<?php
+
+return [
+    'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    'education' => [
+        [
+            'title' => 'Школа',
+            'speciality' => 'Школьник',
+            'yearStart' => '1996',
+            'yearEnd' => '2007',
+        ],
+        [
+            'title' => 'Университет',
+            'speciality' => 'Студент',
+            'yearStart' => '2007',
+            'yearEnd' => '2013',
+        ],
+    ],
+    'experiences' => [
+        [
+            'title' => 'Место работы №1',
+            'post' => 'Должность №1',
+            'city' => 'Санкт-Петерубрг',
+            'company' => 'Компания №1',
+            'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'yearStart' => '2008',
+            'yearEnd' => '2009',
+        ],
+        [
+            'title' => 'Место работы №2',
+            'post' => 'Должность №2',
+            'city' => 'Санкт-Петерубрг',
+            'company' => 'Компания №2',
+            'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'yearStart' => '2009',
+            'yearEnd' => '2012',
+        ],
+        [
+            'title' => 'Место работы №3',
+            'post' => 'Должность №3',
+            'city' => 'Санкт-Петерубрг',
+            'company' => 'Компания №3',
+            'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            'yearStart' => '2013',
+            'yearEnd' => '2018',
+        ],
+    ],
+    'projects' => [
+        [
+            'title' => 'Project #1',
+            'link' => 'http://example1.com',
+            'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        ],
+        [
+            'title' => 'Project #3',
+            'link' => 'http://example2.com',
+            'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        ],
+        [
+            'title' => 'Project #3',
+            'link' => 'http://example3.com',
+            'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        ],
+    ],
+    'skills' => [
+        [
+            'title' => 'PHP',
+            'level' => '90',
+        ],
+        [
+            'title' => 'Unix',
+            'level' => '80',
+        ],
+        [
+            'title' => 'JavaScript',
+            'level' => '75',
+        ],
+        [
+            'title' => 'SQL',
+            'level' => '90',
+        ],
+    ],
+];
---
+++ data/profile_data.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'about' => [
+        'name' => 'Павел Ламзин',
+        'photo' => '',
+        'email' => 'pavel.lamzin@yandex.ru',
+        'phone' => '+7(911)123-45-67',
+        'site' => 'example.com',
+        'post' => 'Team Lead PHP / Senior Developer / CTO'
+    ],
+    'education' => [
+        [
+            'title' => 'Школа',
+            'speciality' => 'Школьник',
+            'yearStart' => '1996',
+            'yearEnd' => '2007',
+        ],
+        [
+            'title' => 'Университет',
+            'speciality' => 'Студент',
+            'yearStart' => '2008',
+            'yearEnd' => '2013',
+        ],
+    ],
+    'languages' => [
+        [
+            'title' => 'English',
+            'level' => 'Intermediate C2'
+        ],
+        [
+            'title' => 'Russian',
+            'level' => 'Intermediate C4'
+        ],
+    ],
+    'interests' => [
+        'Гитара',
+        'Мотоциклы',
+        'Программирование'
+    ],
+
+];
